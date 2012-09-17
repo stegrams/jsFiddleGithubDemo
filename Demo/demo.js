073a7d0901eb7dd5d188beb02ae63fc3049ffb09
Stefan <grownal@otenet.gr>
2012-09-17 15:49:50 +0300

    Change Request's url to this repo

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,6 +1,6 @@
 window.addEvent('domready', function() {
 	new Request.HTML({
-		url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
+		url: '/gh/get/response.html/stegrams/jsFiddleGithubDemo/tree/master/Demo/',
 		data: {'delay': 1},
 		method: 'post',
 		update: 'demo',
